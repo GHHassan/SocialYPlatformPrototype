bceacf5e0aa87e1acc5299161c0c3f27dc286b97
Hassan <66774893+GHHassan@users.noreply.github.com>
2024-02-15 10:50:44 +0000

    profile Avatat Structure built

---
+++ App/src/components/utils/ProfileAvatar.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {useNavigate} from 'react-router-dom';
+
+const ProfileAvatar = ({ imagePath, userID, firstName, lastName }) => {
+  
+  const navigate = useNavigate();
+  const hasImage = imagePath !== 'Private' && imagePath.trim() !== '';
+  const fullName = `${firstName} ${lastName}`;
+  const initials = `${firstName.charAt(0)}${lastName.charAt(0)}`;
+  const image = hasImage ? (
+    <img
+      className="object-cover w-full h-full"
+      src={imagePath}
+      alt={fullName}
+    />
+  ) : (
+    <div className="flex items-center justify-center w-full h-full bg-gray-300 text-gray-600 font-semibold text-2xl">
+      {initials}
+    </div>
+  );
+
+  const handleClick = () => {
+    console.log(userID + " is the userID from ProfileAvatar");
+    navigate(`/singleProfile/${userID}`);
+  }
+  return (
+    <div>
+      <div className="relative w-10 h-10 overflow-hidden rounded-full m-2 cursor-pointer"
+        onClick={handleClick}
+      >
+        {image} 
+      </div>
+        <h4>{fullName}</h4>
+    </div>
+  );
+};
+
+export default ProfileAvatar;
