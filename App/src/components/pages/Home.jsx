ee87d47d028883fb96c99bcdb7d507fb7b7474fc
Hassan <66774893+GHHassan@users.noreply.github.com>
2024-02-19 08:53:56 +0000

    Home page cleared- continues work

--- App/src/components/pages/Home.jsx
+++ App/src/components/pages/Home.jsx
@@ -1,36 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import Post from '../pageFractions/Post';
 
 const Home = () => {
 
-    const [posts, setPosts] = useState([]);
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        const fetchPosts = async () => {
-            const res = await fetch('https://jsonplaceholder.typicode.com/posts',
-            { method: "GET" });
-
-            const data = await res.json();
-            setPosts(data);
-            setLoading(false);
-        }
-        fetchPosts();
-    }, []);
     
+
     return (
-        <div>
-            {loading ? <h1>Loading...</h1> :
-                <div >
-                    <h1 className='text-5xl'>HOME</h1>
-                    {posts.map((post) => (
-                        <div key={post.id} className=' bg-slate-500 border-2 round-'>
-                            <h3>{post.title}</h3>
-                            <p>{post.body}</p>
-                        </div>
-                    ))}
-                </div>
-            }
-        </div>
+       <>
+        <Post />
+       </>
     );
 }
 
