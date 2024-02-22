4badfa6825bfbb96a9bf4ccd5718b992c9c85b08
Hassan <66774893+GHHassan@users.noreply.github.com>
2024-02-22 00:55:01 +0000

    header fixed

--- App/src/components/pageFractions/Header.jsx
+++ App/src/components/pageFractions/Header.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = () => {
+const Header = (props) => {
+
+    const signOut = () => {
+        localStorage.removeItem('token');
+        props.setSignedIn(false);
+    }
   return (
     <nav className="bg-gray-800">
         <ul className="flex justify-center space-x-4">
@@ -15,7 +20,11 @@ const Header = () => {
                 <Link to="/settings" className="text-white hover:text-gray-300">Settings</Link>
             </li>
             <li>
-                <Link to="/signIn" className="text-white hover:text-gray-300">Sign In</Link>
+                {props.signedIn ? (
+                    <p className="text-white hover:text-gray-300" onClick={signOut}>Sign Out</p>
+                ) : (
+                    <Link to="/signIn" className="text-white hover:text-gray-300">Sign In</Link>
+                )}
             </li>
         </ul>
     </nav>
